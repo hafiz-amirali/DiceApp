e6103034358bb58c7f85f100d2eaeeb11e07e468
Hafiz Amir Ali <hafizamir588@gmail.com>
2022-12-11 10:54:01 +0500

    First commit

---
+++ app/src/main/java/com/innovatribe/diceapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.innovatribe.diceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
